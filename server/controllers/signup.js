eab1e250ae22b05171b822d8659fb64ff1639460
Shreejith-19 <shreejith1919@gmail.com>
2026-01-06 18:39:09 +0530

    feat: wrote code for createUser

---
+++ server/controllers/signup.js
@@ -0,0 +1,25 @@
+import bcrypt from "bcrypt"
+import db from "../config/db.js"
+
+export async function createUser(req, res){
+    //signup procedure:
+    /**
+     1. get the req.body
+     2. hash the password using bcrypt
+     3. save the user
+    **/
+   try{
+        const {username, email, password} = req.body
+        //input sanitization
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const result = await db.execute("insert into person(person_name, email, password_hash) values(?, ?, ?)", [username, email,   hashedPassword])
+        res.status(201).json({"successMessage": "user saved"})
+   }catch(error){
+        // 409 - duplicate entry
+        // 500 - server error
+        //400 - bad request
+        res.status(500).json({"errorMessage": "error saving the user"})
+        console.error(error)
+   }
+}
