b9f4b8a76f00089982996aff24d90d22bd7976e8
Shreejith-19 <shreejith1919@gmail.com>
2026-01-06 18:37:04 +0530

    feat: create routes fro api/signup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 import dotenv from "dotenv"
 dotenv.config()
 import express from "express"
+
 import { connectDB } from './helper/db_connection.js'
+import signupRouter from "./routes/signupRouter.js"
 
 const PORT = process.env.PORT
 const app = express()
+
+//middlewares
+app.use(express.urlencoded({extended: true}))
+
+//routing
+app.use("/api/signup", signupRouter)
 
 connectDB().then(()=>{
     app.listen(PORT, ()=>{
