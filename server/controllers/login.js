9f195e9a7a3d4ae8e43ba75e66cb6432279f086d
Shreejith-19 <shreejith1919@gmail.com>
2026-01-07 14:55:06 +0530

    feat: write logic for login

---
+++ server/controllers/login.js
@@ -0,0 +1,40 @@
+import dotenv from "dotenv"
+dotenv.config()
+import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt"
+import db from '../config/db.js'
+
+export async function verifyUserAndLogin(req, res){
+    //Login Procedure:
+    /**
+      1. get the req.body
+      2. check if the user exists using the email
+      3. then check the password
+      4. create bearer token with an expiry
+      5. send the bearer token to client with the response
+     **/
+    const {email, password} = req.body
+    //input sanitization
+
+    try{
+        const result = await db.execute("select person_id, password_hash from person where email = ? limit 1", [email])
+        if(result[0].length === 0){
+            res.status(404).json({errorMessage: "wrong email or password"})
+            return
+        }
+        const {person_id, password_hash} = result[0][0]
+        const isMatch = await bcrypt.compare(password, password_hash)
+        if(!isMatch){
+            res.status(404).json({errorMessage: "wrong email or password"})
+            return
+        }
+        const payload = {person_id: person_id, email: email}
+        const bearer_token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {expiresIn: "3h"})
+        res.status(200).json({sucessMessage: "You are logged in", token : bearer_token})
+        return
+    }catch(error){
+        res.status(500).json({"errorMessage": "server error"})
+        console.error(error)
+    }
+    
+}
