4ccdd0f55bd2f0a6ce40994e2079bbb74baca426
Shreejith-19 <shreejith1919@gmail.com>
2026-01-11 17:57:32 +0530

    feat: completed function for creating issue, display members, createnewproject

--- server/routes/projectRouter.js
+++ server/routes/projectRouter.js
@@ -1,7 +1,27 @@
 import express from 'express'
 import { createNewProject } from '../controllers/projects.js'
+import { createNewIssue, deleteIssueById, editIssueById, getAllBasicIssueDetails, getIssueDetailsById } from '../controllers/issues.js'
+import { showAllIssuesByTypeInProject, showAllMembersInProject } from '../controllers/summary.js'
 const router = express.Router()
+
 router.route('/new')
-    .post(createNewProject)
+    .post(createNewProject)//done
+
+router.route("/:projectId/boards")
+    .get(getAllBasicIssueDetails)
+
+router.route('/:projectId/summary/members')
+    .get(showAllMembersInProject)//done
+
+router.route('/:projectId/summary/issues/:issueType')
+    .get(showAllIssuesByTypeInProject)//done
+
+router.route('/:projectId/issues/:issueId')
+    .get(getIssueDetailsById)
+    .patch(editIssueById)
+    .delete(deleteIssueById)
+
+router.route('/:projectId/issues/new')
+    .post(createNewIssue)//done
 
 export default router
