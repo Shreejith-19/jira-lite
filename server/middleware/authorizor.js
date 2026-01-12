15240776be1160981e9168a31a57814e49b7a431
Shreejith-19 <shreejith1919@gmail.com>
2026-01-12 19:54:15 +0530

    created authorization middleware

--- server/middleware/authorizor.js
+++ server/middleware/authorizor.js
@@ -7,8 +7,29 @@
 5. verify the perms
 6. add perms and role to req object
 */
-export function authorizePermission(requiredPerms){
+import db from '../config/db.js'
+
+export function authorizePermission(requiredPerm){
     return async function(req, res, next){
-        
+        const personId = req.user.personId
+        const projectId = req.params.projectId
+        try {
+            const [roleResult] = await db.execute("select role_id from project_person_role where project_id = ? and person_id = ?", [projectId, personId])
+            const roleId = roleResult[0].role_id
+
+            const [permResult] = await db.execute("select p.perm_code from permission p join role_permission rp on p.perm_id = rp.perm_id where role_id = ?",[roleId])
+            const permCodes = permResult.map(item => {return item.perm_code})
+            const isAllowed = requiredPerm.every(perm => permCodes.includes(perm))
+            if(!isAllowed){
+                return res.status(403).json({Message: "Operation not allowed"})
+            }
+            req.projectRole = roleId
+            req.projectPermissions = permCodes
+            next()
+            
+        } catch (error) {
+            console.error(error)
+            return res.status(500).json({errorMessage: "Operation not allowed"})
+        }        
     }
-}
+}       
